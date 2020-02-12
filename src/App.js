35d5b2f47a40fc61a8d1c817624ff66fc077de82
Beth Collins <bethcollinsliv92@gmail.com>
2020-02-12 12:44:19 +0100

    base structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component () {
-  render() 
-};
+class App extends React.Component() {
+  render() {
+    return <div className="App">Feel better in 15</div>;
+  }
+}
 
 export default App;
