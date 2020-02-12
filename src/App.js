4980a8882a7fdd5143fcdabdd93a8a6f618612ae
Beth Collins <bethcollinsliv92@gmail.com>
2020-02-12 17:34:14 +0100

    trying to work out the components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from "react";
 import "./App.css";
 
 import data from "./data.json";
-import Intro from "./components.Intro";
-import selectExercises from "./components.selectExercises";
-import chooseTime from "./components.selectExercises";
+import selectExercises from "./components.selectExercises.js";
+import chooseTime from "./components.chooseTime.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data,
-      exercise: {},
-      selectedHeart: {},
-      selectedMind: {},
-      selectedBody: {}
+      exercise: {}
     };
   }
 
@@ -24,104 +20,12 @@ class App extends React.Component {
     });
   }
 
-  // handleClick(exercise) {
-  //   if(exercise)
-  // }
-
   render() {
     return (
-      <div className="App container">
-        <h1 className="mb-3 mt-3">Feel Better in 15</h1>
-        <p>
-          The Feel Better in 15 program is designed to keep health and
-          well-being simple and accessible to everyone. How many times have you
-          started on a health journey with all the right intentions, but
-          somehow, somewhere you lost your way? You'll be happy to know, you're
-          not a failure, it's the goals and expectations you set yourself that
-          are unsustainable. The Feel Better in 15 program focuses on the three
-          main aspects of health: heart, mind and body. It requires only 15
-          minutes of your time per day, all the exercises are free and can be
-          done from the comfort of your own home.{" "}
-        </p>
-        <div className="card shadow">
-          <div className="row">
-            <div className="col-md -3 mb-5 mt-5 ml-5">
-              <img src={this.state.exercise.img} className="img-fluid" />
-            </div>
-            <div className="col-md -9 mt-5">
-              <h1>{this.state.exercise.title}</h1>
-              <p>{this.state.exercise.time}</p>
-              <p>{this.state.exercise.description}</p>
-              <p>{this.state.exercise.tips}</p>
-              <button
-                // onClick={() => this.handleClick(this.state.exercise)}
-                type="button"
-                class="btn btn-outline-warning"
-              >
-                Select
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <h4 className="mt-4">Heart</h4>
-          <div className="row mt-5">
-            {this.state.data.heart.map((exercise, index) => (
-              <div
-                key={index}
-                onClick={() => this.showExercise(exercise)}
-                className="col-lg-3 col-md-5 col-sm-6"
-              >
-                <div className="card shadow">
-                  <img src={exercise.url} className="card-img-top" />
-                  <div className="card-body">
-                    <h3 className="card-title">{exercise.title}</h3>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div>
-          <h4>Mind</h4>
-
-          <div className="row mt-5">
-            {this.state.data.mind.map((exercise, index) => (
-              <div
-                key={index}
-                onClick={() => this.showExercise(exercise)}
-                className="col-lg-3 col-md-5 col-sm-6"
-              >
-                <div className="card shadow">
-                  <img src={exercise.url} className="card-img-top" />
-                  <div className="card-body">
-                    <h3 className="card-title">{exercise.title}</h3>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div>
-          <h4>Body</h4>
-          <div className="row mt-5">
-            {this.state.data.body.map((exercise, index) => (
-              <div
-                key={index}
-                onClick={() => this.showExercise(exercise)}
-                className="col-lg-3 col-md-5 col-sm-6"
-              >
-                <div className="card shadow">
-                  <img src={exercise.url} className="card-img-top" />
-                  <div className="card-body">
-                    <h3 className="card-title">{exercise.title}</h3>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+      <div>
+        <h1>Feel Better in 15</h1>
+        <selectExercises />
+        <chooseTime />
       </div>
     );
   }
--- src/components/selectExercises.js
+++ src/components/selectExercises.js
@@ -1,7 +1,102 @@
 import React, { Component } from "react";
 
 export default class selectExercises extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exercise: {}
+    };
+  }
+
+  showExercise(exercise) {
+    this.setState({
+      exercise: exercise
+    });
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <button>Choose your time</button>
+        <div className="card shadow">
+          <div className="row">
+            <div className="col-md -3 mb-5 mt-5 ml-5">
+              <img src={this.props.exercise.img} className="img-fluid" />
+            </div>
+            <div className="col-md -9 mt-5">
+              <h1>{this.props.exercise.title}</h1>
+              <p>{this.props.exercise.time}</p>
+              <p>{this.props.exercise.description}</p>
+              <p>{this.props.exercise.tips}</p>
+              <button
+                // onClick={() => this.handleClick(this.state.exercise)}
+                type="button"
+                class="btn btn-outline-warning"
+              >
+                Select
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h4 className="mt-4">Heart</h4>
+          <div className="row mt-5">
+            {this.props.data.heart.map((exercise, index) => (
+              <div
+                key={index}
+                onClick={() => this.showExercise(exercise)}
+                className="col-lg-3 col-md-5 col-sm-6"
+              >
+                <div className="card shadow">
+                  <img src={exercise.url} className="card-img-top" />
+                  <div className="card-body">
+                    <h3 className="card-title">{exercise.title}</h3>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h4>Mind</h4>
+
+          <div className="row mt-5">
+            {this.props.data.mind.map((exercise, index) => (
+              <div
+                key={index}
+                onClick={() => this.showExercise(exercise)}
+                className="col-lg-3 col-md-5 col-sm-6"
+              >
+                <div className="card shadow">
+                  <img src={exercise.url} className="card-img-top" />
+                  <div className="card-body">
+                    <h3 className="card-title">{exercise.title}</h3>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h4>Body</h4>
+          <div className="row mt-5">
+            {this.props.data.body.map((exercise, index) => (
+              <div
+                key={index}
+                onClick={() => this.showExercise(exercise)}
+                className="col-lg-3 col-md-5 col-sm-6"
+              >
+                <div className="card shadow">
+                  <img src={exercise.url} className="card-img-top" />
+                  <div className="card-body">
+                    <h3 className="card-title">{exercise.title}</h3>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
