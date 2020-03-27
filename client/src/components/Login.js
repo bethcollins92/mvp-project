9585676c6875fa287ed3d791befedcca765a0ab9
Beth Collins <bethcollinsliv92@gmail.com>
2020-03-27 11:10:54 +0100

    adding form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,5 +4,34 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  return <h1>Hello I am the login page</h1>;
+  return (
+    <div className="container">
+      <div className="card">
+        <div class="form-group card-body">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          ></input>
+          <small id="emailHelp" class="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div class="form-group card-body">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          ></input>
+        </div>
+
+        <button type="submit" class="btn btn-primary ">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
